32ef29103be23941dbe924073cda219b24f9e664
Quentin Rampteau <quentin425@hotmail.fr>
2022-05-19 14:44:05 +0200

    Creating Footer component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router } from 'react-router-dom'
 import Home from './pages/Home'
 import "./styles/Index.css"
+import Header from "./components/Header"
+import Footer from './components/Footer'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <Home />
+    <Router>
+      <Header />
+      <Home />
+      <Footer />
+    </Router>
   </React.StrictMode>
 )
